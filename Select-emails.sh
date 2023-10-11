9c79fb3d66a95663c3056c9041ae718433317dee
Deolinda1506 <g.bogore@alustudent.com>
2023-10-11 07:56:37 -0700

    main logic

--- Select-emails.sh
+++ Select-emails.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-# This is my file 
+# This is my file
--- main.sh
+++ main.sh
@@ -1,2 +1,18 @@
 #!/usr/bin/env bash
 # This is my file
+
+# Loop for students registration
+while true; do
+	clear
+	echo "ALU Students Registration"
+	echo "1. Create Student Record"
+	echo "2. Save Student Record"
+	echo "3. View All Students"
+	echo "4. Delete Student Record"
+	echo "5. Update Student Record"
+	echo "6. Exit" 
+
+	# Allow the user to choose options
+	read -p "Choose (1/2/3/4/5/6):  " choice
+
+	# Handle user's choice
